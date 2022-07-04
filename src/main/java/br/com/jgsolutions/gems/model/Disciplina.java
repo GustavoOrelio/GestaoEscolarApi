e9b860b553cd600e25aaa22b4400cadca358deeb
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-04 20:24:29 -0300

    Desenvolvendo crud de disciplina

---
+++ src/main/java/br/com/jgsolutions/gems/model/Disciplina.java
@@ -0,0 +1,36 @@
+package br.com.jgsolutions.gems.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "disciplina")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Getter
+@Setter
+public class Disciplina implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nomeDisciplina;
+
+
+}
+
